960a2f61bb917de4818a3da2a0de48d31d141090
Rodrigo Lujan <rlujan@hiberus.com>
2020-02-20 19:54:23 +0100

    no message

--- HashCode.Entities/Library.cs
+++ HashCode.Entities/Library.cs
@@ -4,11 +4,11 @@ namespace HashCode.Entities
 {
     public class Library
     {
-        public IList<Book> Books { get; set; }
+        public LinkedList<Book> Books { get; set; }
 
         public int Days { get; set; }
 
-        public int NumberOfBooks { get; set; }
+        public int NumberOfBooksCanScanPerDay { get; set; }
 
         public int Time { get; set; }
     }
--- HashCode/Program.cs
+++ HashCode/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using HashCode.Entities;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +12,79 @@ namespace HashCode
     {
         static void Main(string[] args)
         {
+            var path = @"D:\proyectos\.NET\HashCode2020-Hiberus\HashCode\Files\a_example.txt";
+
+            var fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            var sr = new StreamReader(fs, Encoding.ASCII);
+
+            // General
+
+            LinkedList<Book> bookList = new LinkedList<Book>();
+            LinkedList<Library> libraryList = new LinkedList<Library>();
+
+            // Leemos la línea 1
+
+            string line1 = sr.ReadLine();
+            List<string> line1Array = line1.Split(' ').ToList();
+
+            string numBooks = line1Array[0];
+            string numLibraries = line1Array[1];
+            string timeToScan = line1Array[2];
+
+            // Leemos la línea 2
+
+            string line2 = sr.ReadLine();
+            List<string> scoreList = line2.Split(' ').ToList();
+
+            for (int index = 0; index < scoreList.Count; index++)
+            {
+                Book book = new Book {
+                    Id = index,
+                    Score = int.Parse(scoreList[index])
+                };
+
+                bookList.AddLast(book);
+            }
+
+
+            // Leemos el resto de líneas con librerías
+
+            string otherLine = String.Empty;
+
+            while ((otherLine = sr.ReadLine()) != null)
+            {
+                LinkedList<Book> libraryBooks = new LinkedList<Book>();
+
+                List<string> libraryLine1 = otherLine.Split(' ').ToList();
+
+                string libraryLine2 = sr.ReadLine();
+                string[] bookListInLibrary = libraryLine2.Split(' ');
+
+                foreach (string bookInLibrary in bookListInLibrary)
+                {
+                    int index = int.Parse(bookInLibrary);
+                    Book book = GetBook(bookList, index);
+                    libraryBooks.AddLast(book);
+                }
+
+                Library library = new Library {
+                    Books = libraryBooks,
+                    Time = int.Parse(libraryLine1[1]),
+                    NumberOfBooksCanScanPerDay = int.Parse(libraryLine1[2])
+                };
+
+                libraryList.AddLast(library);
+            }
 
             // Para evitar cerrar la consola
             Console.ReadLine();
+        }
+
+        public static Book GetBook(LinkedList<Book> bookList, int index)
+        {
+            return bookList
+                .Where(book => book.Id.Equals(index))
+                .First();
         }
     }
 }
